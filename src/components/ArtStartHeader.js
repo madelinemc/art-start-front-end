9d4dc96eab454f57cce03ecb419b996f74240bbb
madelinemc <madeline.coons@gmail.com>
2021-03-02 11:32:04 -0600

    added logo png and rendered in header

---
+++ src/components/ArtStartHeader.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class ArtStartHeader extends React.Component{
+
+    render(){
+        return(
+            <div id="art-start-header">
+                <img 
+                    src="./public/art-start-logo.png"
+                    alt="met-museum-with-art-start-logo" 
+                />
+            </div>
+        )
+    }
+
+}
+
+export default ArtStartHeader
