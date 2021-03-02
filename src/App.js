1777de50508878e43a612d2ddb34cabf98295800
madelinemc <madeline.coons@gmail.com>
2021-03-02 11:14:18 -0600

    added react-router-dom and updated npm, created component files and in app.js listed routes with components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowswerRouter as Router, Switch } from 'react-router-dom';
 import './App.css';
+
+import ArtStartHeader from './components/ArtStartHeader';
+import ArtContainer from './components/ArtContainer';
+import ArtShow from './components/ArtShow';
+import SearchBar from './components/SearchBar';
+import DepartmentList from './components/DepartmentList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ArtStartHeader />
+
+      <Switch>
+
+        <Route exact path="/artwork">
+          <ArtContainer />
+        </Route>
+        <Route path="/artwork/:id">
+          <ArtShow />
+        </Route>
+
+        <Route exact path="/search">
+          <SearchBar />
+        </Route>
+        <Route path="/search/:keyword/artwork">
+          <ArtContainer />
+        </Route>
+
+        <Route exact path="/departments">
+          <DepartmentList />
+        </Route>
+        <Route path="/departments/:id/artwork">
+          <ArtContainer />
+        </Route>
+
+      </Switch>
+
     </div>
   );
 }
