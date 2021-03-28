e7476741a9f1e2fe9c8ea65bd1a86e55f2319f66
madelinemc <madeline.coons@gmail.com>
2021-03-28 14:17:24 -0500

    added search bar jsx and css and font awesome for fa fa search

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
+
 class SearchBar extends React.Component{
 
     render(){
         return(
-            <div id="search-bar">
-                This is the search bar
+            <div className="search-wrap">
+                <div className="search-bar">
+                    <input type="text" className="search-term" placeholder="Search By Artist Name">
+                    </input>
+                    <button type="submit" className="search-button">
+                        <i className="fa fa-search"></i>
+                    </button>
+                </div>
             </div>
+
         )
     }
 
